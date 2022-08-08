0249014ac9bb0d8a893345eec2383a7eb8339803
akchindo <abdullahichindo16@gmail.com>
2022-08-08 17:24:32 +0100

    few more changes

---
+++ availability.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+<style type="text/css">
+*
+{ 
+  margin: 0;
+  padding: 0;
+
+}
+
+.top
+{
+  background-color: red;
+  width: 100%;
+  height: 70px;
+  text-align: center;
+  font-size: 40px;
+  font-family: cursive;
+  
+
+}
+
+.top2
+{
+  background-color: black;
+  width: 100%
+  height: 70px;
+  text-align: center;
+  font-size: 40px;
+  font-family: cursive;
+  color:white;
+  margin-top: 0;
+  
+}
+section
+{
+  padding:0
+}
+
+table
+{
+  
+  border-collapse: collapse;
+  text-align: center;
+  margin-right: auto;
+  margin-left: auto;
+}
+
+	
+@media screen and (max-width: 600px) 
+{
+  
+}
+
+
+@media screen and (max-width: 600px) 
+{
+  
+}
+
+</style>
+
+</head>
+<body>
+	<section>
+   <div class="top">
+     <p>WE VALUE THE PEOPLE BEHIND THE PLATE</p></div>
+     <div class="top2" ">
+     <p>HOME-DELIGHT</p>
+     <p><?php
+date_default_timezone_set("Africa/Lagos");
+echo "The time is " . date("h:i:sa");
+?></p>
+   </div>
+   
+  </section>
+  <section >
+    <div style="overflow-x: auto;">
+    <table border="2">
+      <!-- FIRST ROW -->
+      <tr>
+        <td width="400px" height="400px"><img src="eba.jpg" width="400px" height="400px"></td><td width="400px" height="400px"><img src="danwake.jpg" width="400px" height="400px"></td width="400px" height="400px"><td><img src="masa.jpg" width="400px" height="400px"></td>
+      </tr>
+      <tr>
+        <td>
+          EBA 
+                 </td><td>
+          DAN-WAKE 
+        </td><td>MASA 
+          
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <a href="tel:08034500951">place order</a>
+        </td>
+        <td>
+          <a href="tel:08034500951">place order</a>
+        </td>
+        <td>
+          <a href="tel:08034500951">place order</a>
+        </td>
+      </tr>
+      <!-- SECOND ROW -->
+      <tr>
+        <td width="400px" height="300px"><img src="jollof.jpg" width="400px" height="300px"></td><td width="400px" height="300px"><img src="moi-moi.jpg" width="400px" height="300px"></td width="400px" height="300px"><td><img src="tuwo-kuka.jpg" width="400px" height="300px"></td>
+      </tr>
+      <tr>
+        <td>
+          JOLLOF-RICE 
+                 </td><td>
+          MOI-MOI 
+        </td><td>KUKA 
+          
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <a href="tel:08034500951">place order</a>
+        </td>
+        <td>
+          <a href="tel:08034500951">place order</a>
+        </td>
+        <td>
+          <a href="tel:08034500951">place order</a>
+        </td>
+      </tr>
+    </table>
+    </div>
+  </section>
+
+</body>
+</html>
